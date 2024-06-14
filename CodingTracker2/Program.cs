862ebc2587e40308f60b0ebcc710e9dedbb1e56b
akeelu47 <106173287+akeelu47@users.noreply.github.com>
2024-06-14 14:40:50 +0100

    Duplicated message

--- CodingTracker2/Program.cs
+++ CodingTracker2/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C#!!! Hello");
+Console.WriteLine("Hello, C#!!! Hello");
+Console.WriteLine("Hello, C#!!! Hello");
+Console.WriteLine("Hello, C#!!! Hello");
